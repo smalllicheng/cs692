09639040334b3f08e61942da1e4262a5a8d5875f
shaurya <shauryakamle@umass.edu>
2020-09-14 02:13:31 -0400

    Updating readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # cs692
+Clone this repo and do cd cs692/
 Clone and build google/benchmark 
 Clone and build zstd
 
--- zstd-benchmark.cpp
+++ zstd-benchmark.cpp
@@ -5,51 +5,56 @@
 #include <cuda.h>
 #include "zstd.h"
 
-using namespace std; 
-float * arr;
+using namespace std;
+float *arr;
 
-// __global__ some_kernel() {
+// Kernel function to run the compression on the GPU.
+__global__ static void some_kernel()
+{
+}
 
-// }
-
-static void BM_cuda(benchmark::State& state) {
-	for (auto _ : state) {
-	int size = sizeof(float)*4096;
-	arr = (float *)cudaMalloc(size);
-
+/*
+*   Benchmark function responsible to run compression on GPU memory.
+*/
+static void BM_cuda(benchmark::State &state)
+{
+    for (auto _ : state) {
+        int size = sizeof(float) * 4096;
+        arr = (float *)cudaMalloc(size);
         size_t const compressed_buffer_size = ZSTD_compressBound(size);
-        // std::cout << "Original size is " << size << " " << "New size is " << compressed_buffer_size << endl;
         void *const compressed = cudaMalloc(compressed_buffer_size);
-        size_t const cSize = ZSTD_compress(compressed,compressed_buffer_size,(void*)arr,size,1);
+        size_t const cSize = ZSTD_compress(compressed, compressed_buffer_size, (void *)arr, size, 1);
         cout << "Actual compressed size is: " << cSize << endl;
-        size_t const dSize = ZSTD_decompress((void *)arr, size, compressed ,cSize);
+        size_t const dSize = ZSTD_decompress((void *)arr, size, compressed, cSize);
         cout << "Decompressed size is: " << dSize << endl;
         cudaFree(arr);
         cudaFree(compressed);
-
-	}
-}
-
-static void BM_func(benchmark::State& state) {
-    
-    for (auto _ : state) {
-	 int size = sizeof(float)*4096;
-    arr = (float *)malloc(size);
-
-        size_t const compressed_buffer_size = ZSTD_compressBound(size);
-	// std::cout << "Original size is " << size << " " << "New size is " << compressed_buffer_size << endl;
-        void *const compressed = malloc(compressed_buffer_size);
-        size_t const cSize = ZSTD_compress(compressed,compressed_buffer_size,(void*)arr,size,1);
-	cout << "Actual compressed size is: " << cSize << endl;
-       	size_t const dSize = ZSTD_decompress((void *)arr, size, compressed ,cSize);
-	cout << "Decompressed size is: " << dSize << endl;
-	free(arr);
-	free(compressed);
     }
-        
 }
 
+/*
+*   Benchmark function to run benchmark on CPU memory.
+*/
+static void BM_func(benchmark::State &state)
+{
+
+    for (auto _ : state) {
+        int size = sizeof(float) * 4096;
+        arr = (float *)malloc(size);
+        size_t const compressed_buffer_size = ZSTD_compressBound(size);
+        void *const compressed = malloc(compressed_buffer_size);
+        size_t const cSize = ZSTD_compress(compressed, compressed_buffer_size, (void *)arr, size, 1);
+        cout << "Actual compressed size is: " << cSize << endl;
+        size_t const dSize = ZSTD_decompress((void *)arr, size, compressed, cSize);
+        cout << "Decompressed size is: " << dSize << endl;
+        free(arr);
+        free(compressed);
+    }
+}
+
+// Registration of the benchmark functions.
 BENCHMARK(BM_func);
 BENCHMARK(BM_cuda);
 
+// Run the benchmarks.
 BENCHMARK_MAIN();
