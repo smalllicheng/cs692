6762de93177cf76066f79786568edf06396f34db
Shaurya Patel <shauryapatel1995@gmail.com>
2020-10-15 08:42:39 +0000

    Updating compress

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ export PATH=/usr/local/cuda-11.0/bin:$PATH
 g++ zstd-benchmark.cpp -std=c++11 -I /usr/local/cuda/ -I /usr/local/cuda/include -I./zstd/lib -I./zstd/ -isystem benchmark/include -Lbenchmark/build/src -L./zstd/lib -L/usr/local/cuda/lib64 -lcudart -lzstd  -lbenchmark -lpthread  -o mybenchmark
 
 Clone and build Nvidia/nvcomp according to instructions the their repo
+Also add cuda and nvcomp to LD preload path.
 g++ -I /usr/local/cuda/ -I /usr/local/cuda/include -I./nvcomp/build/include compress-benchmark.cpp -o compress -lcudart -lnvcomp -L/usr/local/cuda/lib64 -L./nvcomp/build/lib 
 
 Windows - 
--- compress-benchmark.cpp
+++ compress-benchmark.cpp
@@ -1,4 +1,4 @@
-#include "cascaded.hpp"
+#include "lz4.hpp"
 #include "nvcomp.hpp"
 #include <iostream>
 #include <chrono>
@@ -10,43 +10,44 @@ typedef std::chrono::high_resolution_clock Clock;
 
 int main() {
 	// Run a benchmark on the compress function of nvcc
-	size_t size = sizeof(int)*409600000;
+	size_t size = sizeof(int)*4096;
 	int * uncompressed_data;
-	cudaMalloc(reinterpret_cast<void**>(&uncompressed_data),sizeof(int)*409600000);
+	cudaMalloc(reinterpret_cast<void**>(&uncompressed_data),sizeof(int)*4096);
 	
 	 auto t1 = Clock::now();
 
-	int * cpu_data = (int *)malloc(sizeof(int)*409600000);
-	cudaMemcpy((void*)cpu_data, uncompressed_data, 409600000, cudaMemcpyDeviceToHost);
+	int * cpu_data = (int *)malloc(sizeof(int)*4096);
+	cudaMemcpy((void*)cpu_data, uncompressed_data, 4096, cudaMemcpyDeviceToHost);
 	
-	cudaMemcpy((void*)uncompressed_data, cpu_data, 409600000, cudaMemcpyHostToDevice);
+	cudaMemcpy((void*)uncompressed_data, cpu_data, 4096, cudaMemcpyHostToDevice);
 	
 	 auto t2 = Clock::now();
 	std::cout << "Copy done in " << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " ns"  << std::endl;
 	cudaFree(uncompressed_data);
 	free(cpu_data);
-	int uncompressed_count = 409600000;
-
-	nvcomp::CascadedCompressor<int> compressor(
-    	uncompressed_data, uncompressed_count, 2, 1, true);
-	const size_t temp_size = compressor.get_temp_size();
-
-	void * temp_space;
-	cudaMalloc(&temp_space, temp_size);	
 	
-	size_t output_size = compressor.get_max_output_size(
+
+        nvcomp::LZ4Compressor<uint32_t> compressor(
+        (unsigned int *)uncompressed_data, 4096, 65536);
+        const size_t temp_size = compressor.get_temp_size();
+
+        void * temp_space;
+        cudaMalloc(&temp_space, temp_size);
+
+        size_t output_size = compressor.get_max_output_size(
     temp_space, temp_size);
 
-	void * output_space;
-	cudaMalloc(&output_space, output_size);
-	
+        void * output_space;
+        cudaMalloc(&output_space, output_size);
 
-	cudaStream_t stream;
+
+        cudaStream_t stream;
 
 cudaError_t cudaErr = cudaStreamCreate(&stream);
-	nvcompError_t status;
+        nvcompError_t status;
 compressor.compress_async(temp_space,
     temp_size, output_space, &output_size, stream);
+
 
 	std::cout << "Done" << std::endl;
 
